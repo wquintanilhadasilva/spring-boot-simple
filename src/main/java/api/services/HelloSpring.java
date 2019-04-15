e4802cfb97e0657db1fbebacc953e135385acab6
Wedson Quintanilha da Silva <wquintanilhadasilva@gmail.com>
2019-04-15 13:40:20 -0300

    update dependencies

--- src/main/java/api/services/HelloSpring.java
+++ src/main/java/api/services/HelloSpring.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import domain.Pong;
@@ -18,6 +19,7 @@ public class HelloSpring {
 	public static final Logger logger = LoggerFactory.getLogger(HelloSpring.class);
 	
 	@RequestMapping(value = "/ping", method = RequestMethod.GET)
+	@ResponseBody
     public ResponseEntity<Pong> ping() {
         return new ResponseEntity<Pong>(new Pong("Ping received in server!"), HttpStatus.OK);
     }
